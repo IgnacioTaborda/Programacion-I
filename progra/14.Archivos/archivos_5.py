4637c133b9719595a9c3f7b85a2ff615024a372f
IgnacioTaborda <tabordaignacio1@gmail.com>
2025-06-17 18:18:20 -0300

    tttt

--- progra/14.Archivos/archivos_5.py
+++ progra/14.Archivos/archivos_5.py
@@ -14,3 +14,10 @@ personas = [
         "apellido" : "Perrito"
     }
 ]
+
+diccionario = {
+    "nombres" : personas
+}
+
+with open('./progra/14.Archivos/hola.json',"w",encoding='utf-8') as file:
+    json.dump(diccionario,file,indent=4)
